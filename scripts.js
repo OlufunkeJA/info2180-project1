acf29259e4b443b080775a7768b42e8ecb299c93
Olufunke Ogunde <olufunkeogunde@outlook.com>
2025-10-31 17:27:13 -0500

    Added JS functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function(){
+    msgDiv = document.querySelector("div.message");
+    btn = document.querySelector("button.btn");
+
+    btn.addEventListener("click", function(event){
+        //Prevents page refresh
+        event.preventDefault();
+
+        var input = document.querySelector("input").value;
+        var text = "";
+
+        if (input === ""){
+            text = "<p>Please enter a valid email address.</p>";
+            msgDiv.innerHTML = text;
+        }
+        else{
+            text = "<p>Thank you!</p> <p>Your email address '" + input + "' has been added to our mailing list!</p>";
+            msgDiv.innerHTML = text;
+        }
+    });
+    
+});
